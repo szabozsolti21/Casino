1a0f786d14e5400d98bae12512b135738e3417a7
Zsolt <szabzsolt1021@gmail.com>
2022-06-17 00:16:24 +0200

    blackjack p

--- Casino/BlackJack.cs
+++ Casino/BlackJack.cs
@@ -52,8 +52,23 @@ namespace Casino
 
             List<String> dealerHand = new List<String>();
 
-            while (true)
+            int f = 0;
+
+            while(true)
             {
+                if (f != 0)
+                {
+                    Console.WriteLine("\nSzeretnél játszani még  egy kört?\n  (I: Igen / N: Nem)\n");
+                    String nR = Console.ReadLine();
+                    if (nR == "N")
+                    {
+                        Console.WriteLine("Viszlát!");
+                        break;
+                    }
+                }
+
+                f++;
+
                 //Kezek kiürítése
                 userHand.Clear();
                 dealerHand.Clear();
@@ -64,7 +79,7 @@ namespace Casino
                 Console.WriteLine("Add meg a téted!");
                 string betAsString = Console.ReadLine();
                 int bet = int.Parse(betAsString);
-                if(bet > Program.balance)
+                if(bet > prize)
                 {
                     Console.WriteLine("A tét meghaladja az egyenleged!\n");
                     continue;
@@ -88,18 +103,86 @@ namespace Casino
 
                 Console.WriteLine("\nOsztó lapjai: " + dealerHand[0] + " (Rejtett lap)\n Érték: " + dealerHandValue);
 
+                String p;
+                int bj = 0;
+
+                //Ha blackjacket kap kézbe a játékos
+                if (CalculateSum(userHand) == 21)
+                {
+                    Console.WriteLine("**BlackJacket kaptál!**");
+                    p = "S";
+                    bj = 1;
+                }
+
+                //Lehetséges lépések
+                else
+                {
+                    Console.WriteLine("\nLehetsőségek:\n   D: Double (Duplázom a tétem)\n   H: Hit (Kérek még egy lapot)\n   S: Stand (Megállok)");
+                    p = Console.ReadLine();
+                }
 
 
-                //Lehetséges lépések 
-                Console.WriteLine("\nLehetsőségek:\n   D: Double (Duplázom a tétem)\n   H: Hit (Kérek még egy lapot)\n   S: Stand (Megállok)");
-                String p = Console.ReadLine();
+                //Stand
+                if (p == "S")
+                {
+                    if(bj == 0) Console.WriteLine("\nMegálltál");
+
+                    Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
+                    Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + "\n Érték: " + CalculateSum(dealerHand));
+
+                    while(CalculateSum(dealerHand) < 17)
+                    {
+                        dealerHand.Add(GetCard(decks));
+                        decks.Remove(dealerHand[dealerHand.Count-1]);
+                        
+                        Console.WriteLine("\nAz osztónak húznia kellett még egy lapot");
+                        Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
+                        Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + "\n Érték: " + CalculateSum(dealerHand));
+                    }
+
+                    if (CalculateSum(userHand) > 21)
+                    {
+                        Console.WriteLine("\nBust!\n  A veszteséged: " + bet);
+                        Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
+                    }
+                    if (CalculateSum(dealerHand) > 21)
+                    {
+                        Console.WriteLine("\nDealer Bust!\n  A nyereményed: " + bet * 2);
+                        prize += bet * 2;
+                        Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
+                    }
+                    if (CalculateSum(userHand) > CalculateSum(dealerHand))
+                    {
+                        Console.WriteLine("\nNyertél!\n  A nyereményed: " + bet * 2);
+                        prize += bet * 2;
+                        Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
+                    }
+                    if (CalculateSum(userHand) == CalculateSum(dealerHand))
+                    {
+                        Console.WriteLine("\nPush!\n  A nyereményed: " + bet);
+                        prize += bet;
+                        Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
+                    }
+                    if (CalculateSum(userHand) < CalculateSum(dealerHand))
+                    {
+                        Console.WriteLine("\nVesztettél!\n  A veszteséged: " + bet);
+                        continue;
+                    }
+                }//Stand vége
+
 
                 //Double
                 if (p == "D")
                 {
                     Console.WriteLine("\nTét duplázva");
+
                     prize -= bet;
                     bet = bet * 2;
+                    Console.WriteLine("  Új tét: "+bet);
                     userHand.Add(GetCard(decks));
                     decks.Remove(userHand[userHand.Count - 1]);
 
@@ -110,6 +193,24 @@ namespace Casino
                     {
                         Console.WriteLine("\nBust!\n  A veszteséged: " + bet);
                         Console.WriteLine("Az egyenleged a kör után: "+prize);
+                        continue;
+                    }
+
+                    while (CalculateSum(dealerHand) < 17)
+                    {
+                        dealerHand.Add(GetCard(decks));
+                        decks.Remove(dealerHand[dealerHand.Count - 1]);
+
+                        Console.WriteLine("\nAz osztónak húznia kellett még egy lapot");
+                        Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
+                        Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + "\n Érték: " + CalculateSum(dealerHand));
+                    }
+
+                    if (CalculateSum(dealerHand) > 21)
+                    {
+                        Console.WriteLine("\nDealer Bust!\n  A nyereményed: " + bet * 2);
+                        prize += bet * 2;
+                        Console.WriteLine("Az egyenleged a kör után: " + prize);
                         continue;
                     }
                     if (CalculateSum(userHand) > CalculateSum(dealerHand))
@@ -132,58 +233,80 @@ namespace Casino
                         Console.WriteLine("Az egyenleged a kör után: " + prize);
                         continue;
                     }
-                }
-                //Stand
-                if (p == "S")
+                }//Double vége
+
+                
+                //Hit
+                if (p == "H")
                 {
-                    Console.WriteLine("\nMegálltál");
-
-                    Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
-                    Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + "\n Érték: " + CalculateSum(dealerHand));
-
-
-                    if (CalculateSum(userHand) > 21)
+                    while(CalculateSum(userHand) < 21 && p != "S")
                     {
-                        Console.WriteLine("\nBust!\n  A veszteséged: " + bet);
+                        Console.WriteLine("\nKértél még egy lapot");
+
+                        userHand.Add(GetCard(decks));
+                        decks.Remove(userHand[userHand.Count - 1]);
+
+                        Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
+                        Console.WriteLine("\nOsztó lapjai: " + dealerHand[0] + " (Rejtett lap)\n Érték: " + dealerHandValue);
+
+                        if (CalculateSum(userHand) > 21)
+                        {
+                            Console.WriteLine("Bust! A veszteséged: " + bet);
+                            Console.WriteLine("Az osztó lapjai: " + PrintHand(dealerHand) + "voltak.");
+                            
+                            break;
+                        }
+
+                        if(CalculateSum(userHand) == 21)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSzeretnél még egy lapot?\n(H: Hit / S: Stand)");
+                            p = Console.ReadLine();
+                        }
+
+                    }
+
+                    if (CalculateSum(userHand) > 21) continue;
+
+                    while (CalculateSum(dealerHand) < 17)
+                    {
+                        dealerHand.Add(GetCard(decks));
+                        decks.Remove(dealerHand[dealerHand.Count - 1]);
+
+                        Console.WriteLine("\nAz osztónak húznia kellett még egy lapot");
+                        Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
+                        Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + "\n Érték: " + CalculateSum(dealerHand));
+                    }
+
+                    if (CalculateSum(dealerHand) > 21)
+                    {
+                        Console.WriteLine("\nDealer Bust!\n  A nyereményed: " + bet * 2);
+                        prize += bet * 2;
                         Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
                     }
                     if (CalculateSum(userHand) > CalculateSum(dealerHand))
                     {
                         Console.WriteLine("\nNyertél!\n  A nyereményed: " + bet * 2);
                         prize += bet * 2;
                         Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
                     }
                     if (CalculateSum(userHand) == CalculateSum(dealerHand))
                     {
                         Console.WriteLine("\nPush!\n  A nyereményed: " + bet);
                         prize += bet;
                         Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
                     }
                     if (CalculateSum(userHand) < CalculateSum(dealerHand))
                     {
                         Console.WriteLine("\nVesztettél!\n  A veszteséged: " + bet);
-                        Console.WriteLine("Az egyenleged a kör után: " + prize);
+                        continue;
                     }
-                    continue;
-                }
-
-                //Hit
-                if (p == "H")
-                {
-                    Console.WriteLine("\nKértél még egy lapot");
-
-                    userHand.Add(GetCard(decks));
-                    decks.Remove(userHand[userHand.Count - 1]);
-
-                    Console.WriteLine("\nLapjaid: " + PrintHand(userHand) + "\n Érték: " + CalculateSum(userHand));
-
-                    Console.WriteLine("\nOsztó lapjai: " + PrintHand(dealerHand) + " \n Érték: " + CalculateSum(dealerHand));
-
-                    if (CalculateSum(userHand) > 21)
-                    {
-                        Console.WriteLine("");
-                    }
-
 
                 }
                 
@@ -191,6 +314,7 @@ namespace Casino
                 if(!CheckIfValidP(p)){
                     Console.WriteLine("\nKérlek a három lehetőség közül válassz!");
                 }
+
             }
 
             
@@ -234,9 +358,15 @@ namespace Casino
                 }
                 else
                 {
+                    if(sum == 10 && card[1] == 'A')
+                    {
+                        sum += 11;
+                        break;
+                    }
                     if(sum == 20 && card[1] == 'A')
                     {
                         sum++;
+                        break;
                     }
                     sum += 10;
                 }
